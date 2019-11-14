9ab2e2b93d53eff39e1bc5cc180b20960ed21250
kamakshijain <kamakshi@terpmail.umd.edu>
2019-11-14 18:50:29 -0500

    Implemented the walkerAlgorithm class

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@
  */
 
 int main(int argc, char* argv[]) {
+    //Initialize the ROS node
     ros::init(argc, argv, "walkerAlgorithm");
+
+     //Create the object for class walkerAlgorithm
+    WalkerAlgorithm walker;
+
+     // Implement the walker behaviour 
+    walker.runTurtlebot();
+
     return 0;
 }
