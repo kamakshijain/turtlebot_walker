c92368782871f38e22d3e16fa7b0c9fc333cc5f6
kamakshijain <kamakshi@terpmail.umd.edu>
2019-11-14 01:30:34 -0500

    Added walkerAlgorithm stub implementation

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "walkerAlgorithm.hpp"
+
+/**
+ * @brief      main function
+ *
+ * @param[in]  argc  The argc
+ * @param      argv  The argv
+ *
+ * @return     nothing
+ */
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "walkerAlgorithm");
+    return 0;
+}
---
+++ src/walkerAlgorithm.cpp
@@ -0,0 +1,16 @@
+#include "walkerAlgorithm.hpp"
+
+WalkerAlgorithm::WalkerAlgorithm() {
+}
+
+WalkerAlgorithm::~WalkerAlgorithm() {
+}
+
+void WalkerAlgorithm::laserScannerCallback(cont sensor_msgs::LaserScan::ConstPtr& input) {
+}
+
+bool WalkerAlgorithm::checkObstacle() {
+}
+
+void WalkerAlgorithm::runTurtlebot() {
+}
